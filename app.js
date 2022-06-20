f0fa4fafe474dc855eefcef561ad98e888a9234f
PlanetNamekTech <sr23513@gmail.com>
2022-06-19 20:41:04 -0700

    Responsive website practice

---
+++ app.js
@@ -0,0 +1,7 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navList = document.querySelector('.nav__list');
+
+navToggle.addEventListener('click', () => {
+    navList.classList.toggle('active')
+})
+
